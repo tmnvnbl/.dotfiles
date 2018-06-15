13432d9f561517642d1d8a417033dc106ea28f8a
tmnvnbl <40273626+tmnvnbl@users.noreply.github.com>
2018-06-15 14:01:59 +0200

    generic expression for all dotfiles in ./dotfiles dir

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 My personal dotfiles
 
 This is a work in progress, trying to script everything by hand,
-going from example to general expression doing everything
+going from example to general expression for everything.
+I will try to keep the install script as easy as possible,
+with comments for explanation and examples
 
+
+# Installation instructions
 install.sh installs everything by
 1. making a backup of your current dotfiles to archive
-2. making a symbolic link in your home folder to dotfiles in this repo
+2. making a copy of each file in ./dotfiles to your home folder
 
 
--- install.sh
+++ install.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
 # a script to "install" the dotfiles from this repo into your user folder.
-# This will create symlinks from your user folder to this repo
-# e.g. ~/.vimrc will point to .dotfiles/.vimrc
+# This will copy the dotfiles from this repo to your home folder,
+# while making a backup in ./archive
 
 # TODO: first check if the dotfile already exists in user folder -> make a copy in archive
 # for now: make copy by default, version by date
 export REPO=~/.dotfiles
+export REPO_DOTFILES=$(find dotfiles -type f)
 export HOME=~
 
-echo "REPO=" $REPO
-echo "REPO_DOTFILES=" $REPO_DOTFILES
-echo "HOME=" $HOME
+echo "Installing the following dotfiles:"
+echo $REPO_DOTFILES
+echo "into " $HOME
 
-# for file in $REPO_DOTFILES
-# do
-# 	cp $HOME/$file REPO/archive/$file
-# 	ln -s $REPO/$file $HOME/$file
-# done
+# make an archive folder to backup original dotfiles
+mkdir $REPO/archive
 
-# test with single file
-file=.vimrc
-mv $HOME/$file $REPO/archive/${file}_$(date +%F-%T)
-ln -s $REPO/$file $HOME/$file
+## test with single file
+# file=.vimrc
+# mv $HOME/$file $REPO/archive/${file}_$(date +%F-%T)
+# ln -s $REPO/$file $HOME/$file
+
+for full_path_to_file in $REPO_DOTFILES
+do
+	name=$(basename "$full_path_to_file")
+	cp $HOME/$name $REPO/archive/${name}.$(date +%F-%T)
+	rm $HOME/$name
+	cp  $full_path_to_file $HOME/$name
+done
 
 
